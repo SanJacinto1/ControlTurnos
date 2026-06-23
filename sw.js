e93e92c7c0e70e0d8483cae31b1107185bb785e9
SanJacinto1 <sanjacinto.efactura@gmail.com>
2026-06-23 14:45:39 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'control-turno-v3';
+const CACHE = 'control-turno-v4';
 const ARCHIVOS = ['./', 'index.html', 'style.css', 'logic.js', 'app.js', 'logo.svg', 'icon.svg', 'manifest.json'];
 
 self.addEventListener('install', event => {
